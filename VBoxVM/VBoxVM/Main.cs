5edbd13f251a5525edd3e3edef6d71591a5b944e
Arkneid <chaminejoao@gmail.com>
2025-03-20 00:06:19 +0000

    Add verifications to text box empty and file copy

--- VBoxVM/VBoxVM/Main.cs
+++ VBoxVM/VBoxVM/Main.cs
@@ -2,10 +2,12 @@ namespace VBoxVM
 {
     public partial class Main : Form
     {
+        private string xmlFolder;
         public Main()
         {
             InitializeComponent();
             this.txt_folder_path.ReadOnly = true;
+            this.xmlFolder = $@"C:\Users\{Environment.UserName}\.VirtualBox";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -23,7 +25,26 @@ namespace VBoxVM
 
         private void btn_change_Click(object sender, EventArgs e)
         {
-            //TO DO
+            string xmlFile = $@"{this.xmlFolder}\VirtualBox.xml";
+            string xmlFileBck = $@"{this.xmlFolder}\VirtualBox_bck.xml";
+            string newLocation = this.txt_folder_path.Text;
+
+            // Verify if text box is not empty
+            if (newLocation == "")
+            {
+                MessageBox.Show("Text box is empty, please choose folder first!!!", "Error changing default folder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Copy file
+            try 
+            {
+                File.Copy(xmlFile, xmlFileBck);
+            }
+            catch (IOException ioex)
+            {
+                MessageBox.Show(ioex.Message, "Error copying VirtualBox xml file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
