2f66e5afc9cbc996299a57cf062c67ee41c476c8
Arkneid <chaminejoao@gmail.com>
2025-03-24 20:32:58 +0000

    Basic gui done

--- VBoxVM/VBoxVM/Restore.cs
+++ VBoxVM/VBoxVM/Restore.cs
@@ -12,14 +12,26 @@ namespace VBoxVM
 {
     public partial class Restore : Form
     {
+        private string xmlFolder;
         public Restore()
         {
             InitializeComponent();
+            this.xmlFolder = $@"C:\Users\{Environment.UserName}\.VirtualBox";
         }
 
         private void btn_restore_Click(object sender, EventArgs e)
         {
-            //TODO
+            string xmlFile = $@"{this.xmlFolder}\VirtualBox.xml";
+            string xmlFileBck = $@"{this.xmlFolder}\VirtualBox_bck.xml";
+
+            if (!File.Exists(xmlFileBck))
+            {
+                MessageBox.Show("The backup file don't exists!", "Backup File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            File.Delete(xmlFile);
+            File.Move(xmlFileBck, xmlFile);
         }
     }
 }
