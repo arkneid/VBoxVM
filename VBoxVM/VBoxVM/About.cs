d99b40cf1f94fe1b7972a4fc7b3435c70d6c44d7
Arkneid <chaminejoao@gmail.com>
2025-03-29 23:05:32 +0000

    #9 - Add about window to have more info about app

---
+++ VBoxVM/VBoxVM/About.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VBoxVM
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Main WindowMain = new Main();
+
+            this.Hide();
+            WindowMain.Show();
+        }
+
+        private void LinkLabelHome_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process myProcess = new Process();
+            myProcess.StartInfo.UseShellExecute = true;
+            myProcess.StartInfo.FileName = LinkLabelHome.Text;
+            myProcess.Start();
+            myProcess.WaitForExit();
+            myProcess.Close();
+        }
+
+        private void LinkLblGetHelp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process myProcess = new Process();
+            myProcess.StartInfo.UseShellExecute = true;
+            myProcess.StartInfo.FileName = LinkLblGetHelp.Text;
+            myProcess.Start();
+            myProcess.WaitForExit();
+            myProcess.Close();
+        }
+    }
+}
--- VBoxVM/VBoxVM/Main.Designer.cs
+++ VBoxVM/VBoxVM/Main.Designer.cs
@@ -36,6 +36,10 @@
             btn_change = new Button();
             main_label = new Label();
             ntfIconMain = new NotifyIcon(components);
+            MainMenu = new MenuStrip();
+            hElpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            MainMenu.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -52,18 +56,17 @@
             // 
             txt_folder_path.BackColor = SystemColors.HighlightText;
             txt_folder_path.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txt_folder_path.Location = new Point(126, 57);
+            txt_folder_path.Location = new Point(12, 81);
             txt_folder_path.Margin = new Padding(3, 4, 3, 4);
             txt_folder_path.Name = "txt_folder_path";
-            txt_folder_path.Size = new Size(341, 27);
+            txt_folder_path.Size = new Size(454, 27);
             txt_folder_path.TabIndex = 1;
             // 
             // btn_choose_folder
             // 
-            btn_choose_folder.Location = new Point(474, 52);
-            btn_choose_folder.Margin = new Padding(3, 4, 3, 4);
+            btn_choose_folder.Location = new Point(249, 115);
             btn_choose_folder.Name = "btn_choose_folder";
-            btn_choose_folder.Size = new Size(114, 41);
+            btn_choose_folder.Size = new Size(141, 50);
             btn_choose_folder.TabIndex = 2;
             btn_choose_folder.Text = "Choose Folder";
             btn_choose_folder.UseVisualStyleBackColor = true;
@@ -71,9 +74,9 @@
             // 
             // btn_change
             // 
-            btn_change.Location = new Point(226, 91);
+            btn_change.Location = new Point(93, 115);
             btn_change.Name = "btn_change";
-            btn_change.Size = new Size(138, 41);
+            btn_change.Size = new Size(141, 50);
             btn_change.TabIndex = 3;
             btn_change.Text = "Change";
             btn_change.UseVisualStyleBackColor = true;
@@ -83,11 +86,11 @@
             // 
             main_label.Dock = DockStyle.Top;
             main_label.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            main_label.Location = new Point(0, 0);
+            main_label.Location = new Point(0, 24);
             main_label.Name = "main_label";
-            main_label.Size = new Size(597, 53);
+            main_label.Size = new Size(478, 53);
             main_label.TabIndex = 4;
-            main_label.Text = "\r\nVBox default folder";
+            main_label.Text = "VBox default folder";
             main_label.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // ntfIconMain
@@ -97,26 +100,55 @@
             ntfIconMain.Visible = true;
             ntfIconMain.MouseClick += notifyIcon_MouseClick;
             // 
+            // MainMenu
+            // 
+            MainMenu.Items.AddRange(new ToolStripItem[] { hElpToolStripMenuItem });
+            MainMenu.LayoutStyle = ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            MainMenu.Location = new Point(0, 0);
+            MainMenu.Name = "MainMenu";
+            MainMenu.RenderMode = ToolStripRenderMode.Professional;
+            MainMenu.Size = new Size(478, 24);
+            MainMenu.TabIndex = 5;
+            MainMenu.Text = "MainMenu";
+            // 
+            // hElpToolStripMenuItem
+            // 
+            hElpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            hElpToolStripMenuItem.Name = "hElpToolStripMenuItem";
+            hElpToolStripMenuItem.Size = new Size(44, 20);
+            hElpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(180, 22);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlLight;
-            ClientSize = new Size(597, 147);
+            ClientSize = new Size(478, 177);
             Controls.Add(main_label);
             Controls.Add(btn_change);
             Controls.Add(btn_choose_folder);
             Controls.Add(txt_folder_path);
             Controls.Add(label1);
+            Controls.Add(MainMenu);
             Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = MainMenu;
             Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
             Name = "Main";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Main";
+            Text = "VBoxVM";
             Resize += onResizeMain;
+            MainMenu.ResumeLayout(false);
+            MainMenu.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -129,5 +161,8 @@
         private Button btn_change;
         private Label main_label;
         private NotifyIcon ntfIconMain;
+        private MenuStrip MainMenu;
+        private ToolStripMenuItem hElpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
